b3935b142a5c7956db4da874e70c22d54424cd51
Jacopo Pangrazi <jacopopangrazi@gmail.com>
2019-11-12 20:41:07 +0100

    Initial commit

---
+++ javascript/script.js
@@ -0,0 +1,11 @@
+// - prove per il cambio sfondo on scroll
+
+// window.onscroll = () => {
+//   let altezza = window.pageYOffset,
+//     back = document.getElementById("projects"),
+//     text = document.getElementsByClassName("projects-section-header");
+//   if (altezza > 690) {
+//     back.style.backgroundColor = "var(--main-gray)";
+//     text[0].style.color = "#fff";
+//   }
+// };
